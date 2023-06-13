1b942cde8aa7d3ddc2bd05c87efedcb027813596
Boyan <b.dechev@student.fontys.nl>
2023-06-13 19:57:54 +0200

    contact page v1 completed

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -1,11 +1,12 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './Contact.css'
-import staticloop from '../../images/static.mp4';
-import ig from '../../images/ig.png';
-import fb from '../../images/fb.png';
-import yt from '../../images/yt.png';
-import lin from '../../images/lin.png';
+import "./Contact.css";
+import React, { useEffect, useState } from "react";
+import ReactModal from "react-modal";
+import { Link } from "react-router-dom";
+import staticloop from "../../images/static.mp4";
+import ig from "../../images/ig.png";
+import fb from "../../images/fb.png";
+import yt from "../../images/yt.png";
+import lin from "../../images/lin.png";
 import phone from "../../images/phone-g.png";
 import email from "../../images/email.png";
 import bird from "../../images/bird.png";
@@ -13,6 +14,32 @@ import battery from "../../images/battery.mp4";
 import signal from "../../images/signal.png";
 
 function Contact() {
+  const [currentTime, setCurrentTime] = useState(new Date());
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setCurrentTime(new Date());
+    }, 1000);
+
+    return () => clearInterval(timer);
+  }, []);
+
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
+  const copyPhoneNumber = () => {
+    const phoneNumberElement = document.getElementById("phoneNumber");
+    const phoneNumber = phoneNumberElement.textContent;
+    navigator.clipboard.writeText(phoneNumber);
+  };
+
   return (
     <div className="whole-page">
       <div className="tv-static">
@@ -42,8 +69,11 @@ function Contact() {
               <img className="signal-img" src={signal} alt="signal" />
             </div>
 
-            <span className="contacts">Contact</span>
-            <span className="hour">18:34</span>
+            <span className="contacts">Contact me</span>
+            <span className="hour">
+              {currentTime.getHours().toString().padStart(2, "0")}:
+              {currentTime.getMinutes().toString().padStart(2, "0")}
+            </span>
 
             <video className="battery" autoPlay loop muted>
               <source
@@ -56,13 +86,92 @@ function Contact() {
           </header>
 
           <div className="grid">
-            <img className="icon email" src={email} alt="email" />
-            <img className="yt icon" src={yt} alt="yt" />
-            <img className="ig icon" src={ig} alt="ig" />
-            <img className="fb icon" src={fb} alt="fb" />
-            <img className="lin icon" src={lin} alt="lin" />
-            <img className="phone icon" src={phone} alt="phone" />
-            <img className="icon" src={bird} alt="twitter" />
+            <div className="grid-icon">
+              <a
+                href="https://twitter.com"
+                target="_blank"
+                rel="noopener noreferrer">
+                <img className="phone icon" src={phone} alt="phone" />
+              </a>
+              <p className="icon-text">Call me</p>
+            </div>
+
+            <div className="grid-icon-email">
+              <img
+                className="icon email"
+                src={email}
+                alt="email"
+                onClick={openModal}
+              />
+
+              <ReactModal
+                className="phone-modal"
+                isOpen={isModalOpen}
+                onRequestClose={closeModal}
+                contentLabel="Modal">
+                <h1 id="phoneNumber">+45 91 84 16 59</h1>
+                <p>Feel free to call me any day between 11 and 18 o'clock</p>
+                <div className="phone-modal-buttons">
+                  <button className="copy-button" onClick={copyPhoneNumber}>
+                    Copy
+                  </button>
+                  <button onClick={closeModal}>Close</button>
+                </div>
+              </ReactModal>
+
+              <p className="icon-text icon-text-email">Mail me</p>
+            </div>
+
+            <div className="grid-icon">
+              <a
+                href="https://www.youtube.com/@medenka"
+                target="_blank"
+                rel="noopener noreferrer">
+                <img className="yt icon" src={yt} alt="yt" />
+                <p className="icon-text">My YouTube</p>
+              </a>
+            </div>
+
+            <div className="grid-icon">
+              <a
+                href="https://www.instagram.com/vaniomednikarovv/"
+                target="_blank"
+                rel="noopener noreferrer">
+                <img className="ig icon" src={ig} alt="ig" />
+              </a>
+              <p className="icon-text">My Instagram</p>
+            </div>
+
+            <div className="grid-icon">
+              <a
+                href="https://www.linkedin.com/in/ivan-mednikarov-420879158"
+                target="_blank"
+                rel="noopener noreferrer">
+                <img className="lin icon" src={lin} alt="lin" />
+              </a>
+              <p className="icon-text">My LinkedIn</p>
+            </div>
+
+            <div className="grid-icon-email">
+              <a
+                href="https://facebook.com"
+                target="_blank"
+                rel="noopener noreferrer">
+                <img className="fb icon" src={fb} alt="fb" />
+              </a>
+              <p className="icon-text icon-text-fb">My Facebook</p>
+            </div>
+
+            <div className="grid-icon grid-icon-twitter">
+              <a
+                href="https://twitter.com"
+                target="_blank"
+                rel="noopener noreferrer">
+                <img className="icon" src={bird} alt="twitter" />
+              </a>
+              <p className="icon-text">Tweet @ me</p>
+            </div>
+
             <div className="grid-item">.</div>
             <div className="grid-item">. </div>
           </div>
