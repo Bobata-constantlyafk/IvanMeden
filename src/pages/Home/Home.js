b29cac07913e8a0ffb128cbef1fbae8c7f9f29ae
Boyan <b.dechev@student.fontys.nl>
2023-08-16 12:48:52 +0200

    performance fix - preload image

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -2,8 +2,23 @@ import './Home.css';
 import staticloop from "../../images/static.webm";
 import ivan from "../../images/home-page-pic-1.webp";
 import { Link } from "react-router-dom";
+import React, { useEffect, useState } from "react";
 
 function Home() {
+  const [loaded, setLoaded] = useState(false);
+
+  useEffect(() => {
+    const img = new Image();
+    img.src = ivan;
+    img.onload = () => {
+      setLoaded(true);
+    };
+  }, []);
+
+  if (!loaded) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div className="whole-page">
       <div className="tv-static">
